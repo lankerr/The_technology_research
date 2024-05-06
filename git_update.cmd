353de0bbc02bae7d03ed4d694094dc695b797efd
你的名字 <lankerr>
2024-05-07 03:31:09 +0800

    update 20240507D03hour31minute08second

--- git_update.cmd
+++
@@ -1,4 +0,0 @@
-git add . --all
-set timestamp=%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%
-git commit -m "update %timestamp%"
-git push
---
+++ update.cmd
@@ -0,0 +1,12 @@
+@echo off
+for /f "tokens=2 delims==" %%I in ('wmic os get localdatetime /value') do set "dt=%%I"
+set "year=%dt:~0,4%"
+set "month=%dt:~4,2%"
+set "day=%dt:~6,2%"
+set "hour=%dt:~8,2%"
+set "minute=%dt:~10,2%"
+set "second=%dt:~12,2%"
+set "timestamp=%year%%month%%day%D%hour%hour%minute%minute%second%second"
+git add . --all
+git commit -m "update %timestamp%"
+git push
